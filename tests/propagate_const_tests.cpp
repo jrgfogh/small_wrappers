0347b10d8f7f0a5a3203460dd60c88e437b8d341
Jørgen Fogh <jrgfogh@gmail.com>
2024-04-25 19:42:59 +0200

    Formatted propagate_const_tests.cpp, to make the CI pipeline happy.

--- tests/propagate_const_tests.cpp
+++ tests/propagate_const_tests.cpp
@@ -6,37 +6,33 @@
 
 template<typename T>
 concept propagate_const_specialization =
-    std::is_move_constructible_v<T> == std::is_move_constructible_v<typename T::element_type>
-;
-static_assert(propagate_const_specialization<sw::propagate_const<int*>>);
+std::is_move_constructible_v<T> == std::is_move_constructible_v<typename T::element_type>;
+static_assert(propagate_const_specialization<sw::propagate_const<int *>>);
 
-struct call_tester final
-{
+struct call_tester final {
     bool non_const_call = false;
     mutable bool const_call = false;
 
     void call() { non_const_call = true; }
+
     void call() const { const_call = true; }
 };
 
-TEST(PropagateConst, ArrowPropagatesNonConstRawPointer)
-{
+TEST(PropagateConst, ArrowPropagatesNonConstRawPointer) {
     call_tester ct;
-    sw::propagate_const<call_tester*> pc{&ct};
+    sw::propagate_const<call_tester *> pc{&ct};
     pc->call();
     EXPECT_TRUE(ct.non_const_call);
 }
 
-TEST(PropagateConst, ArrowPropagatesConstRawPointer)
-{
+TEST(PropagateConst, ArrowPropagatesConstRawPointer) {
     call_tester ct;
-    sw::propagate_const<call_tester*> const pc{&ct};
+    sw::propagate_const<call_tester *> const pc{&ct};
     pc->call();
     EXPECT_TRUE(ct.const_call);
 }
 
-TEST(PropagateConst, ArrowPropagatesNonConstUniquePointer)
-{
+TEST(PropagateConst, ArrowPropagatesNonConstUniquePointer) {
     auto underlying = std::make_unique<call_tester>();
     call_tester &ct{*underlying};
     sw::propagate_const<std::unique_ptr<call_tester>> pc{std::move(underlying)};
@@ -44,17 +40,15 @@ TEST(PropagateConst, ArrowPropagatesNonConstUniquePointer)
     EXPECT_TRUE(ct.non_const_call);
 }
 
-TEST(PropagateConst, ArrowPropagatesConstUniquePointer)
-{
-    auto underlying= std::make_unique<call_tester>();
+TEST(PropagateConst, ArrowPropagatesConstUniquePointer) {
+    auto underlying = std::make_unique<call_tester>();
     call_tester &ct{*underlying};
     sw::propagate_const<std::unique_ptr<call_tester>> const pc{std::move(underlying)};
     pc->call();
     EXPECT_TRUE(ct.const_call);
 }
 
-TEST(PropagateConst, ArrowPropagatesNonConstSharedPointer)
-{
+TEST(PropagateConst, ArrowPropagatesNonConstSharedPointer) {
     auto underlying = std::make_shared<call_tester>();
     call_tester &ct{*underlying};
     sw::propagate_const<std::shared_ptr<call_tester>> pc{std::move(underlying)};
@@ -62,8 +56,7 @@ TEST(PropagateConst, ArrowPropagatesNonConstSharedPointer)
     EXPECT_TRUE(ct.non_const_call);
 }
 
-TEST(PropagateConst, ArrowPropagatesConstSharedPointer)
-{
+TEST(PropagateConst, ArrowPropagatesConstSharedPointer) {
     auto underlying = std::make_shared<call_tester>();
     call_tester &ct{*underlying};
     sw::propagate_const<std::shared_ptr<call_tester>> const pc{std::move(underlying)};
